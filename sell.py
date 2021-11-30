6931fc392859ed9a8d9f5f22e73f643f631d1b29
Louis Breton <45322259+breton123@users.noreply.github.com>
2021-11-30 15:36:56 +0000

    Add files via upload

--- sell.py
+++ sell.py
@@ -11,27 +11,39 @@ def sell(address,tx):
     balance = web3.eth.get_balance(sender_address)
     contract_id = web3.toChecksumAddress(address)
     sellTokenContract = web3.eth.contract(contract_id, abi=abi.sellabi)
+    decimal = sellTokenContract.functions.decimals().call()
     contract = web3.eth.contract(address=config["PANCAKE_ROUTER_ADDRESS"], abi=abi.panabi)
     balance = sellTokenContract.functions.balanceOf(sender_address).call()
     symbol = sellTokenContract.functions.symbol().call()
     tokenValue = web3.toWei(balance, 'ether')
-    tokenValue2 = web3.fromWei(tokenValue, 'ether')
+    tokenValue2 = str(web3.fromWei(tokenValue, 'ether'))
+    front = tokenValue2[:(len(tokenValue2) - decimal)]
+    back = tokenValue2[(len(tokenValue2) - decimal):]
+    finalValue = front + "." + back
+    finalValue = float(finalValue)
+    finalValue = web3.toWei(finalValue, 'gwei')
     nonce = web3.eth.get_transaction_count(sender_address)
-    nonce = nonce + 1
-    approve = sellTokenContract.functions.approve(config["PANCAKE_ROUTER_ADDRESS"], balance).buildTransaction({
-        'from': sender_address,
-        'gas': config["GAS_AMOUNT"],
-        'gasPrice': web3.toWei(config["GAS_PRICE"], 'gwei'),
-        'nonce': int(nonce),
-        })
-    signed_txn = web3.eth.account.sign_transaction(approve, private_key=config["WALLET_PRIVATE"])
-    tx_token = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
-
-    nonce = nonce + 1
     while True:
         try:
+            nonce = nonce + 1
+            approve = sellTokenContract.functions.approve(config["PANCAKE_ROUTER_ADDRESS"], balance).buildTransaction({
+            'from': sender_address,
+            'gas': config["GAS_AMOUNT"],
+            'gasPrice': web3.toWei(config["GAS_PRICE"], 'gwei'),
+            'nonce': int(nonce),
+            })
+            signed_txn = web3.eth.account.sign_transaction(approve, private_key=config["WALLET_PRIVATE"])
+            tx_token = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
+            break
+        except Exception as e:
+            pass
+    print(colors.Green+f"Approved to sell {symbol}")
+    nonce = web3.eth.get_transaction_count(sender_address)
+    while True:
+        try:
+            nonce = nonce + 1
             pancakeswap2_txn = contract.functions.swapExactTokensForETHSupportingFeeOnTransferTokens(
-                int(tokenValue2),int(config["SLIPPAGE"]), 
+                int(finalValue),int(config["SLIPPAGE"]), 
                 [contract_id,spend],
                 str(sender_address),
                 (int(time.time()) + 10000)
@@ -45,8 +57,7 @@ def sell(address,tx):
             tx_token = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
             break
         except Exception as e:
-            nonce = nonce + 1
-            print(e)
+            pass
     print(colors.Green+f"Successfully Sold {symbol}")
     print(colors.Green+"TX Token: " +str(web3.toHex(tx_token)))
     telegram_send.send(messages=[f"Successfully Sold {symbol}"+"\nTX Token: "+ str(web3.toHex(tx_token))])
